79f27ab4079b14ac7823fec9238d53331a4db2c6
Michael Camilleri <mike@inqk.net>
2014-09-03 15:31:39 +0900

    Moved from Capistrano 2 to Capistrano 3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,46 @@
-default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-set :ssh_options, { :forward_agent => true }
+# config valid only for Capistrano 3.1
+lock '3.2.1'
 
-set :application, "inqk.net"
-set :domain,      "deploy.inqk.net"
-set :repository,  "git@github.com:pyrmont/inqk.git"
-set :deploy_to,   "/var/www/inqk.net"
-set :shared_path, "#{deploy_to}/shared"
-set :use_sudo,    false
- 
-set :scm,        :git
-set :branch,     'master'
-set :deploy_via, :remote_cache
+set :application, 'inqk.net'
+set :repo_url, 'git@github.com:pyrmont/inqk.git'
 
-role :web, domain
-role :app, domain # this can be the same as the web server
-role :db,  domain, :primary => true # this can be the same as the web server
- 
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/var/www/inqk.net'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :info
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{weblog}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart do ; end
-end
 
-namespace :deploy do
-  task :create_symlinks, :roles => :web do
-    run "ln -s #{shared_path}/weblog #{current_release}/weblog"
+  desc 'Restart application'
+  task :restart do
+    puts "This is a static deployment. There is no restart task."
   end
-end
 
-after "deploy:finalize_update", "deploy:create_symlinks"
+  after :publishing, :restart
+
+end
