cfab2c96790841b34710fe845e973070dcfe9d72
Michael Camilleri <mike@inqk.net>
2017-12-20 19:22:31 +0900

    Fix issues with deploying via Capistrano.

--- Capfile
+++ Capfile
@@ -4,6 +4,10 @@ require 'capistrano/setup'
 # Includes default deployment tasks
 require 'capistrano/deploy'
 
+# Add the Git SCM
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
 # Includes tasks from other gems included in your Gemfile
 #
 # For documentation on these, see for example:
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock '3.10.1'
 
 set :application, 'inqk.net'
 set :repo_url, 'git@github.com:pyrmont/inqk.git'
@@ -9,9 +9,6 @@ set :repo_url, 'git@github.com:pyrmont/inqk.git'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/inqk.net'
-
-# Default value for :scm is :git
-# set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
